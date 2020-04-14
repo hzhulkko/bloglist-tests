d2081e2b80bacf1f17a874a2c5a2fb2941a7eba3
Heidi Hulkko <hulkhei@gmail.com>
2020-04-14 16:21:50 +0300

    Add test for user listing

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -32,54 +32,77 @@ describe('Blog app', function() {
       cy.login({username: 'root', password: 'secret'})
     })
 
-    it('all blogs are listed in descending order by likes', function() {
-      cy.get('#blog-list').find('.blog').should('have.length', 6)
-      cy.get('#blog-list').find('.blog')
-        .each(function() {
-          cy.contains('view').click()
-        })
-      cy.get('#blog-list').find('.likes')
-        .each(function(el, index) {
-          const expectedLikesOrder = ['5', '4', '3', '2', '1', '0']
-          const num = el.text().match('\\d+')[0]
-          expect(num).to.eq(expectedLikesOrder[index])
-        })
+    describe('in the front page', function() {
+
+      it('all blogs are listed in descending order by likes', function() {
+        cy.get('#blog-list').find('.blog').should('have.length', 6)
+        cy.get('#blog-list').find('.blog')
+          .each(function() {
+            cy.contains('view').click()
+          })
+        cy.get('#blog-list').find('.likes')
+          .each(function(el, index) {
+            const expectedLikesOrder = ['5', '4', '3', '2', '1', '0']
+            const num = el.text().match('\\d+')[0]
+            expect(num).to.eq(expectedLikesOrder[index])
+          })
+      })
+
+      it('user can view blog details', function() {
+        cy.contains('Title 1').contains('view').click()
+        cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
+        cy.get('.blog').contains('Title 1').parent().contains('url: http://some.url')
+        cy.get('.blog').contains('Title 1').parent().contains('user: Test User')
+      })
+
+      it('user can add a new blog', function() {
+        cy.contains('add new blog').click()
+        cy.get('#title').type('New Blog')
+        cy.get('#author').type('Some Author')
+        cy.get('#url').type('http://some.test.url')
+        cy.get('#add-button').click()
+        cy.get('#blog-list').find('.blog').should('have.length', 7)
+        cy.get('#blog-list').contains('New Blog by Some Author')
+      })
+
+      it('user can remove their blog', function() {
+        cy.contains('Title 2').contains('view').click()
+        cy.get('.blog').contains('Title 2').parent().contains('remove').click()
+        cy.get('.blog').should('not.contain', 'Title 2')
+      })
+
+      it('user cannot remove another users blog', function() {
+        cy.contains('Title 1').contains('view').click()
+        cy.get('.blog').contains('Title 1').parent().should('not.contain', 'remove')
+      })
+
+      it('user can like a blog', function() {
+        cy.contains('Title 1').contains('view').click()
+        cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
+        cy.get('.blog').contains('Title 1').parent().contains('like').click()
+        cy.get('.blog').contains('Title 1').parent().contains('likes: 2')
+      })
+
     })
 
-    it('user can view blog details', function() {
-      cy.contains('Title 1').contains('view').click()
-      cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
-      cy.get('.blog').contains('Title 1').parent().contains('url: http://some.url')
-      cy.get('.blog').contains('Title 1').parent().contains('user: Test User')
+    describe('in the users page', function() {
+
+      beforeEach(function() {
+        cy.visit('http://localhost:3000/users')
+      })
+
+      it.only('all users and number of blogs they have created are shown', function() {
+        cy.get('#user-list').find('tbody tr').should('have.length', 3)
+        cy.get('#user-list').contains('Superuser').parent().find('td').eq(1).contains(3)
+        cy.get('#user-list').contains('Test User').parent().find('td').eq(1).contains(3)
+        cy.get('#user-list').contains('Login User').parent().find('td').eq(1).contains(0)
+
+
+      })
+
     })
 
-    it('user can add a new blog', function() {
-      cy.contains('add new blog').click()
-      cy.get('#title').type('New Blog')
-      cy.get('#author').type('Some Author')
-      cy.get('#url').type('http://some.test.url')
-      cy.get('#add-button').click()
-      cy.get('#blog-list').find('.blog').should('have.length', 7)
-      cy.get('#blog-list').contains('New Blog by Some Author')
-    })
 
-    it('user can remove their blog', function() {
-      cy.contains('Title 2').contains('view').click()
-      cy.get('.blog').contains('Title 2').parent().contains('remove').click()
-      cy.get('.blog').should('not.contain', 'Title 2')
-    })
-
-    it('user cannot remove another users blog', function() {
-      cy.contains('Title 1').contains('view').click()
-      cy.get('.blog').contains('Title 1').parent().should('not.contain', 'remove')
-    })
-
-    it('user can like a blog', function() {
-      cy.contains('Title 1').contains('view').click()
-      cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
-      cy.get('.blog').contains('Title 1').parent().contains('like').click()
-      cy.get('.blog').contains('Title 1').parent().contains('likes: 2')
-    })
   })
 
 
