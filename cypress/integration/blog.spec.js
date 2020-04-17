6ce797345a68c79b77ac50c47bf93bb9f4139906
Heidi Hulkko <hulkhei@gmail.com>
2020-04-17 10:52:31 +0300

    Update tests

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -48,24 +48,21 @@ describe('Blog app', function() {
     describe('in the front page', function() {
 
       it('all blogs are listed in descending order by likes', function() {
-        cy.get('#blog-list').find('.blog').should('have.length', 6)
-        cy.get('#blog-list').find('.blog')
-          .each(function() {
-            cy.contains('view').click()
-          })
-        cy.get('#blog-list').find('.likes')
+        cy.get('#blog-list').find('a').should('have.length', 6)
+        cy.get('#blog-list').find('a')
           .each(function(el, index) {
-            const expectedLikesOrder = ['5', '4', '3', '2', '1', '0']
+            const expectedTitleOrder = ['5', '4', '3', '2', '1', '6']
             const num = el.text().match('\\d+')[0]
-            expect(num).to.eq(expectedLikesOrder[index])
+            expect(num).to.eq(expectedTitleOrder[index])
           })
       })
 
-      it('user can view blog details', function() {
-        cy.contains('Title 1').contains('view').click()
+      it('user can view blog details in blog page by clicking the link', function() {
+        cy.contains('Title 1').click()
         cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
         cy.get('.blog').contains('Title 1').parent().contains('url: http://some.url')
         cy.get('.blog').contains('Title 1').parent().contains('user: Test User')
+        cy.url().should('match', /blogs\/[a-z0-9]{24,}/)
       })
 
       it('user can add a new blog', function() {
@@ -74,23 +71,24 @@ describe('Blog app', function() {
         cy.get('#author').type('Some Author')
         cy.get('#url').type('http://some.test.url')
         cy.get('#add-button').click()
-        cy.get('#blog-list').find('.blog').should('have.length', 7)
+        cy.get('#blog-list').find('a').should('have.length', 7)
         cy.get('#blog-list').contains('New Blog by Some Author')
       })
 
       it('user can remove their blog', function() {
-        cy.contains('Title 2').contains('view').click()
+        cy.contains('Title 2').click()
         cy.get('.blog').contains('Title 2').parent().contains('remove').click()
-        cy.get('.blog').should('not.contain', 'Title 2')
+        cy.url().should('be', 'http://localhost:3000/')
+        cy.get('#blog-list').should('not.contain', 'Title 2')
       })
 
       it('user cannot remove another users blog', function() {
-        cy.contains('Title 1').contains('view').click()
+        cy.contains('Title 1').click()
         cy.get('.blog').contains('Title 1').parent().should('not.contain', 'remove')
       })
 
       it('user can like a blog', function() {
-        cy.contains('Title 1').contains('view').click()
+        cy.contains('Title 1').click()
         cy.get('.blog').contains('Title 1').parent().contains('likes: 1')
         cy.get('.blog').contains('Title 1').parent().contains('like').click()
         cy.get('.blog').contains('Title 1').parent().contains('likes: 2')
@@ -114,13 +112,13 @@ describe('Blog app', function() {
       it('clicking on a name shows blogs added by the user', function() {
         cy.contains('Test User').click()
         cy.get('h3').contains('Test User')
-        cy.get('#user-blog-list').find('.blog').should('have.length', 3)
-        cy.get('#user-blog-list').find('.blog').contains('Title 1')
-        cy.get('#user-blog-list').find('.blog').contains('Title 3')
-        cy.get('#user-blog-list').find('.blog').contains('Title 5')
+        cy.get('#user-blog-list').find('a').should('have.length', 3)
+        cy.get('#user-blog-list').find('a').contains('Title 1')
+        cy.get('#user-blog-list').find('a').contains('Title 3')
+        cy.get('#user-blog-list').find('a').contains('Title 5')
       })
 
-      it('if the user has not added blogs, message is dispalyed', function() {
+      it('if the user has not added blogs, message is displayed', function() {
         cy.contains('Login User').click()
         cy.get('h3').contains('Login User')
         cy.contains('No blogs added')
